bb330302d87e7287b7e3ef87b12f21bb027bcd07
sheva <shevchuksanya983@gmail.com>
2019-02-18 06:45:56 +0200

    fixed bugs, in general everything works, but i ended up with shit code a bit, limp code

--- app.js
+++ app.js
@@ -18,17 +18,25 @@ reset();
 
 clearBtn.addEventListener('click', reset);
 
+// delete previous symbol 
 deleteSymbol.addEventListener('click', function(){
-    if(Number.isInteger(Number(inputPanel.innerHTML[length-1]))){
+    if(Number.isInteger(Number(inputPanel.innerHTML[inputPanel.innerHTML.length-1]))){
         if(secondNumber) secondNumber = secondNumber.substr(0, secondNumber.length-1);
         else firstNumber = firstNumber.substr(0, firstNumber.length-1);
-    }
+    } else sign="";
     inputPanel.innerHTML = inputPanel.innerHTML.substr(0, inputPanel.innerHTML.length-1);
-    sign="";
+    
 });
 
+/**
+ * if we do not type 0 and put dot '.' automatically it will fill field with 0.
+ */
 function digitsListener(item, i){
     item.addEventListener('click', function(){
+        if(digits[i].textContent==='.'){
+            if(!sign && firstNumber.indexOf('.')>-1) return;
+            if(sign && secondNumber.indexOf('.')>-1) return;
+        }
         if((!firstNumber && digits[i].textContent==='.') ||
         (sign && !secondNumber && digits[i].textContent==='.')) {
             inputPanel.innerHTML+="0";
@@ -43,6 +51,9 @@ function digitsListener(item, i){
     })
 };
 
+/**
+ * if there is empty field we can not type any sign except minus
+ */
 function signListener(item, i){
     item.addEventListener('click', function(){
         if(firstNumber){
